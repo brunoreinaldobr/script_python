0f67560c2bef9b293e9b0c3a2a094dbd2c78aad7
Bruno <you@example.com>
2015-06-30 09:47:23 -0300

    Primeiro commit

---
+++ run_valgrind.py
@@ -0,0 +1,78 @@
+__author__ = 'bruno'
+
+# coding=UTF-8
+
+# import sys
+import os
+
+# images_filelist = sys.argv[2]
+# print images_filelist
+
+VALGRIND_CMD = 'valgrind'
+
+JPEG_PATH = '/opt/jpeg-9a/'
+JPEG_CMD = 'cjpeg'
+IMAGES_PATH = JPEG_PATH + 'imagens/'
+
+IMAGES_LIST = [
+    'bolas.BMP',
+    'disneyBMP.bmp',
+    'RAY.BMP',
+    'LAND2.BMP',
+    'land.BMP',
+    'LAND.BMP',
+    'test_pattern_original.bmp',
+    'metal.bmp'
+]
+
+OUTPUT_LIST = []
+
+# valgrind --tool=cachegrind --cachegrind-out-file=jpeg.txt /opt/jpeg-9a/cjpeg -outfile /opt/jpeg-9a/imagens/saida.jpg entrada.jpg
+
+# ----------------------------------------------------------------------------
+
+cont = 1
+y = 0
+for image in IMAGES_LIST:
+    command = 'touch jpeg' + str(cont) + '.txt && '
+    command += VALGRIND_CMD + ' --tool=cachegrind '
+    command += '--cachegrind-out-file=jpeg' + str(cont) + '.txt '  # saida do valgrind para cada imagem
+    command += JPEG_PATH + JPEG_CMD
+    command += ' -outfile ' + IMAGES_PATH + image[:-3] + 'jpg '
+    command += IMAGES_PATH + image
+
+    print (command)
+
+    os.system(command)
+    # ---------------------------------------------------------------------------
+    arq = open('jpeg' + str(cont) + '.txt', 'r')
+    lista = arq.readlines()
+    arq.close()
+    string = ""
+    # string = "Execution;Ir;Dr;Dw;I1mr;D1mr;D1mw;I2mr;D2mr;D2mw;\n"
+    for i in range(0, len(lista)):
+        if 'summary' in lista[i]:
+            image = IMAGES_LIST[y]
+            # print image
+            string += "jpeg_" + image[:-4] + ";"
+            arq_sum = open('summary.txt', 'r')
+            l = arq_sum.readlines()
+            arq_sum.close()
+            arq_sum = open('summary.txt', 'w')
+            line = lista[i].split(" ")
+            del line[0]  # apaga a palavra "summary:"
+            word = line[-1]
+            word = list(word)
+            del word[-1]
+            line[-1] = ''.join(word)
+            for w in range(0, len(line)):
+                if w == (len(line) - 1):
+                    string += line[w] + ";\n"
+                else:
+                    string += line[w] + ";"
+            l.append(string)
+            arq_sum.writelines(l)
+    arq_sum.close()
+    cont += 1
+    y += 1
+
---
+++ run_x265.py
@@ -0,0 +1,79 @@
+__author__ = 'Bruno Reinaldo'
+
+# coding=UTF-8
+
+import os
+
+VALGRIND_CMD = 'valgrind'
+
+X265_PATH = '/home/bruno/x265/build/linux/'
+X265_CMD = './x265'
+VIDEOS_PATH = X265_PATH + 'videos/'
+
+VIDEOS_LIST = [
+    'BasketballDrive_1920x1080_50.yuv',
+    'BQSquare_416x240_60.yuv'
+    'BQTerrace_1920x1080_60.yuv',
+    'Cactus_1920x1080_50.yuv',
+    'Kimono1_1920x1080_24.yuv',
+    'Traffic_2560x1600_30.yuv'
+]
+
+SEARCH_ALGORITHM = [
+    'hex',
+    'star'
+]
+
+SEARCH_AREA = [
+    '32',
+    '64',
+    '96',
+    '128'
+]
+
+OUTPUT_LIST = []
+# cd /home/bruno/x265/build/linux/
+# valgrind --tool=cachegrind --cachegrind-out-file=video.txt ./x265 --input entrada.yuv --fps number_fps 
+# --input-res LarguraxAltura --seek first_frame_to_encode --frames number_frames --me <hex> --merange --output saida.x265
+
+for algorithm in SEARCH_ALGORITHM:
+    for search in SEARCH_AREA:
+        cont = 1
+        for video in VIDEOS_LIST:
+            command = 'cd ' + X265_PATH + ' && '
+            command += VALGRIND_CMD + ' --tool=cachegrind '
+            command += '--cachegrind-out-file=video' + str(cont) + '.txt '
+            command += X265_CMD
+            command += ' --input ' + VIDEOS_PATH + video + ' --fps ' + video[-6:-4] + ' --input-res '
+            p = video.split('_')
+            command += p[1] + ' --frames 1 --me ' + algorithm + ' --merange ' + search + ' --output ' + p[0] + '.265'
+            print (command)
+            os.system(command)
+
+            arq = open(X265_PATH + 'video' + str(cont) + '.txt', 'r')
+            lista = arq.readlines()
+            arq.close()
+            string = ""
+            for i in range(0, len(lista)):
+                if 'summary' in lista[i]:
+                    string = "x265_" + p[0] + '_' + algorithm + '_' + search + ';'
+                    print (video)
+                    arq_sum = open('summary.txt', 'r')
+                    l = arq_sum.readlines()
+                    arq_sum.close()
+                    arq_sum = open('summary.txt', 'w')
+                    line = lista[i].split(" ")
+                    del line[0]
+                    word = line[-1]
+                    word = list(word)
+                    del word[-1]
+                    line[-1] = ''.join(word)
+                    for w in range(0, len(line)):
+                        if w == (len(line) - 1):
+                            string += line[w] + ";\n"
+                        else:
+                            string += line[w] + ";"
+                    l.append(string)
+                    arq_sum.writelines(l)
+                    arq_sum.close()
+            cont += 1
